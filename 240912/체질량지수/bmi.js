61747f0e1e4c3b393cd94a865f405dff4fe5a9f0
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-12 08:09:13 +0900

    Title: 체질량지수, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 240912/체질량지수/bmi.js
+++ 240912/체질량지수/bmi.js
@@ -4,10 +4,9 @@ let input = fs.readFileSync(0).toString().split(" ");
 let num1 = Number(input[0])
 let num2 = Number(input[1])
 
-
-    let bmi = Math.floor(((10000*num2) / (num1*num1)));
-    if(bmi >= 25) {
-        console.log(`${bmi}\nObesity`)
-    } else {
-        console.og(bmi);
-    }
+let bmi = Math.floor(((10000*num2) / (num1*num1)));
+if(bmi >= 25) {
+    console.log(`${bmi}\nObesity`)
+} else {
+    console.log(bmi);
+}
