3fb0126a42ff0840f675594c55fe2c7f15c3b7d8
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-12 08:13:50 +0900

    Title: 정사각형의 넓이, Time: 176ms, Memory: 14MB, Status: Passed - CodeTree

--- 240912/정사각형의 넓이/area-of-a-rectangle.js
+++ 240912/정사각형의 넓이/area-of-a-rectangle.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().split(" ");
 
-let result = input < 5 ? console.log(`tiny\n${input*input}`) : console.log(`${input*input}`);
+let result = input < 5 ? console.log(`${input*input}\ntiny`) : console.log(`${input*input}`);
