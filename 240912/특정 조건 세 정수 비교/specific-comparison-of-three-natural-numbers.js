e97d630251ab02652f142fe2b481a36211b45397
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-12 09:53:33 +0900

    Title: 특정 조건 세 정수 비교, Time: 201ms, Memory: 17MB, Status: Passed - CodeTree

--- 240912/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240912/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -6,7 +6,7 @@ let num2 = Number(input[1]);
 let num3 = Number(input[2]);
 
 let ans1, ans2;
-if(num1 < num2 && num1 < num3) { //최소값 구하는 곳
+if(num1 <= num2 && num1 <= num3) { //최소값 구하는 곳
     ans1 = 1;
 } else {
     ans1 =0;
