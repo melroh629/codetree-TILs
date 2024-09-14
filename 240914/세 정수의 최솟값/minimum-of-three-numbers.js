d6fa52bb23256835d1cccb79b74b716be6447a15
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-14 14:36:30 +0900

    Title: 세 정수의 최솟값, Time: 234ms, Memory: 47MB, Status: Passed - CodeTree

--- 240914/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 240914/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -3,7 +3,7 @@ let input = fs.readFileSync(0).toString().split(" ");
 
 let a = Number(input[0]);
 let b = Number(input[1]);
-let c = Number(input[1]);
+let c = Number(input[2]);
 
 
 let minNumber;
