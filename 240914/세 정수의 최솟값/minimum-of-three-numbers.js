d9825370c0957a5f898bc6a310b6e35052765b1e
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-14 14:35:56 +0900

    Title: 세 정수의 최솟값, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240914/세 정수의 최솟값/minimum-of-three-numbers.js
+++ 240914/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().split(" ");
 
-let num1 = Number(input[0]);
-let num2 = Number(input[1]);
-let num3 = Number(input[1]);
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[1]);
 
-// 정렬로 숫자를 오름차순으로 정리한다.
-// 오름차순에서의 가장 첫번째 숫자
 
-let sorted = input.sort((a,b) => a - b);
+let minNumber;
+if(a <= b && a <= c ) minNumber = a;
+else if( b <= c && b <= a) minNumber = b;
+else minNumber = c;
 
-console.log(sorted[0])
+console.log(minNumber)
