73c6326a5dd14d7b8d43874937b56ae2cd3c87ff
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-15 14:13:07 +0900

    Title: 최댓값 구하기, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240915/최댓값 구하기/maximum-value.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+
+if( a > b) {
+    if(a > c) console.log(a)
+    else console.log(c)
+} else if ( b > c ) {
+    console.log(b)
+} console.log(c)
