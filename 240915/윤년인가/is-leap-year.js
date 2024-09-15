e9249189d45b31d6b2999bee9d56e4e014f2eb73
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-15 12:21:35 +0900

    Title: 윤년인가, Time: 194ms, Memory: 14MB, Status: Passed - CodeTree

--- 240915/윤년인가/is-leap-year.js
+++ 240915/윤년인가/is-leap-year.js
@@ -6,5 +6,5 @@ input = Number(input);
 
 if(input % 4 === 0) {
     if((input % 100 === 0) && (input % 400 !==0)) console.log('false')
-    console.log('true')
+    else console.log('true')
 } else console.log('false')
