db663a27e138ea2b42734c384ae64c9b95f1748d
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-17 12:43:54 +0900

    Title: 문자 출력하기, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

--- 240917/문자 출력하기/print-text.js
+++ 240917/문자 출력하기/print-text.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
-let input =fs.readFileSync(0).toString();
+let input =fs.readFileSync(0).toString().trim();
 
 let result = '';
 for(let i=1; i<=8; i++){
-    result += `${input}`
+    result +=`${input}`
 }
 
 console.log(result)
