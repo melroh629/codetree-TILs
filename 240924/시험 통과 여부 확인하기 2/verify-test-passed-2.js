ab7e0fbd8d755047c58120f36c48132a00fe8891
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-24 10:09:22 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 188ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240924/시험 통과 여부 확인하기 2/verify-test-passed-2.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split("\n");
+let counts = Number(input[0]);
+let student1 = input[1].split(" ").map(Number);
+let student2 = input[2].split(" ").map(Number);
+let student3 = input[3].split(" ").map(Number);
+
+let pass = 0;
+
+const getAverage = array => {
+    let sum = array.reduce((acc, curr) => acc + curr);
+    let average = sum / array.length;
+    
+    return average;
+}
+
+const getGrade = array => {
+    let score = getAverage(array);
+    if(score >= 60) {
+        console.log('pass');
+        pass++;
+    } else {
+        console.log('fail');
+    }
+}
+getGrade(student1);
+getGrade(student2);
+getGrade(student3);
+console.log(pass);
