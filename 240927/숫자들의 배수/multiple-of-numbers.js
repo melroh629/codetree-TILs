455070ed1c9050d9af150c988f3c8a299aaa920c
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-27 10:56:30 +0900

    Title: 숫자들의 배수, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 240927/숫자들의 배수/multiple-of-numbers.js
+++ 240927/숫자들의 배수/multiple-of-numbers.js
@@ -1,21 +1,25 @@
+// 변수 선언 및 입력
 const fs = require("fs");
-let number = fs.readFileSync(0).toString().trim();
-number = Number(number)
+let n = Number(fs.readFileSync(0).toString().trim());
 
-let array = Array(10);
-// array[0] = number;
+let arr = Array(10);
+let cnt = 0;
 
-for(let i=1; i<=10; i++){
-    array[i-1] =  number * i;
+// 입력받은 정수의 배수를 배열에 저장
+arr[0] = n;
+for (let i = 1; i < 10; i++) {
+    arr[i] = arr[i - 1] + arr[0];
 }
+
+// 배열의 원소들을 출력하다가 5의 배수가 2번 나오면 출력을 멈춤
 let str = "";
-let count = 0;
-for(let j=0; j<array.length; j++){
-    if(array[j] % 5 === 0){
-        str += `${array[j]} `;
-        count++;
+
+for (let i = 0; i < 10; i++) {
+    str += arr[i] + " ";
+    if (arr[i] % 5 === 0) {
+        cnt++;
     }
-    if(count ===2){
+    if (cnt >= 2) {
         break;
     }
 }
