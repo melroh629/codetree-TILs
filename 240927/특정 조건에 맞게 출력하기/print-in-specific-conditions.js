2bb7ea31cebd4026a24998709cfac7b8496adb4a
Jungwoo Melissa Roh <58014235+melroh629@users.noreply.github.com>
2024-09-27 12:45:37 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240927/특정 조건에 맞게 출력하기/print-in-specific-conditions.js
+++ 240927/특정 조건에 맞게 출력하기/print-in-specific-conditions.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 let str = "";
 for(let i=0; i<input.length; i++){
-    if(input[i] === 0) str+= ""
+    if(input[i] === 0) break;
     if(input[i] !== 0 && input[i] % 2 === 0) {
         str += `${input[i] / 2} `
     } else if(input[i] !== 0 && input[i] % 2 !== 0) {
